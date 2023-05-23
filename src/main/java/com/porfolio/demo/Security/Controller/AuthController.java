6e55bdebb5f1b5a811107b9793009a79a0a66076
Georqx <sputnikuno@outlook.com>
2023-05-23 13:05:35 -0300

    mas pruebas cross origin

--- src/main/java/com/porfolio/demo/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/demo/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontend-porfolio-gl.web.app")
+@CrossOrigin(origins = "https://frontend-porfolio-gl.web.app/**")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
